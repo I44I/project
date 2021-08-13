86aea62a24e648e6d7ca051b1d0f4ca92d36ea62
Ruslan <xxx081103xxx@mail.ru>
2021-08-13 15:13:09 +0300

    js mod

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
+const number0fFilms = +prompt ("Сколько фильмов вы уже посммотрели?");
+const personalMovieDB = {
+    count:number0fFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
 
+};
+
+const a = prompt("Один из послдених просмотренных фильмов?",''),
+b = prompt('На сколько оцените его?',''),
+c = prompt ('Один из послдених просмотренных фильмов?',''),
+d = prompt('На сколько оцените его?','');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
