db3f0decf1cd32b77d60e3369f26ef5d522589f8
Ruslan <xxx081103xxx@mail.ru>
2021-08-18 13:10:16 +0300

    modification

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+if (4 == 9) {
+    console.log("Ok!");
+
+}else {
+    console.log('error');
+}
+
+const num = 50;
+if (num<49){
+    console.log('Error');
+}else if (num>100){
+    console.log('Много');
+}else{
+    console.log('Ok!');
+}
+
+(num == 50) ? console.log('Ok!')
